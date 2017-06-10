3e4cb4125233df1d52b1f23c81d728dd501903a7
happymaj00r <gameforlife20@hotmail.de>
2017-06-11 01:08:49 +0200

    Xin FIx
    Xin Fix

--- Xin/Xin/Menu.cs
+++ Xin/Xin/Menu.cs
@@ -42,7 +42,10 @@ namespace Xin
             LaneClearMenu.Add(new MenuCheckbox("useQLC", "Use Q", true));
             LaneClearMenu.Add(new MenuCheckbox("useELC", "Use E", true));
            
-
+            JungleClearMenu = MainMenu.AddSubMenu("Lane Clear Menu");
+            JungleClearMenu.Add(new MenuCheckbox("useQV", "Use Q", true));
+            JungleClearMenu.Add(new MenuCheckbox("useEV", "Use E", true));
+            JungleClearMenu.Add(new MenuCheckbox("useWV", "Use W", true));
             
 
             /* Killsteal Section */
---
+++ Xin/Xin/Modes/JungleClear.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using HesaEngine.SDK;
+using static Xin.MenuLoader;
+using static Xin.SpellLoader;
+
+namespace Xin.Modes
+{
+
+    public class JungleClear
+    {
+
+        public static void ActivatedLaneClear()
+        {
+
+            var q = LaneClearMenu.Get<MenuCheckbox>("useQV").Checked && Q.IsReady();
+            var e = LaneClearMenu.Get<MenuCheckbox>("useEV").Checked && E.IsReady();
+            var w = LaneClearMenu.Get<MenuCheckbox>("useWV").Checked && W.IsReady();
+
+
+
+            var minion = ObjectManager.MinionsAndMonsters.NeutralCamps.Where(x => x.IsValidTarget(Q.Range));
+
+            foreach (var m in minion)
+            {
+                if ( m.IsValidTarget() && !m.IsDead && e)
+                {
+                 
+                            
+                            {
+                                E.Cast(m);
+                            }
+                
+                }
+                
+                
+                if ( m.IsValidTarget() && !m.IsDead && q)
+                {
+                 
+                            
+                    {
+                        Q.Cast();
+                    }
+                
+                }
+                
+                if ( m.IsValidTarget() && !m.IsDead && w)
+                {
+                 
+                            
+                    {
+                        W.Cast();
+                    }
+                
+                }
+                
+                
+                
+                
+            }
+        }
+    }
+}
